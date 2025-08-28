18ee2f2ea6b89134b6dd65ed602fa775a0e2dffe
trompeterm <trompeterm@msoe.edu>
2025-08-28 15:52:36 -0500

    chat completion done

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,19 @@
 from tools.transcript import Transcript
 from tools.chunker import Chunker
 from tools.embeddings import Embeddings
+from tools.chat_completion import ChatCompletion
 
 def main():
     transcript = Transcript()
     chunker = Chunker()
     embedder = Embeddings()
-    text = transcript.get_transcript("dQw4w9WgXcQ")
-    chunks = chunker.chunk_text(text, 10)
+    chat_completion = ChatCompletion()
+    text = transcript.get_transcript("LPZh9BOjkQs")
+    chunks = chunker.chunk_text(text, 100)
     vectors = embedder.embed_text(chunks)
     index = embedder.build_index(vectors)
-    results = embedder.search("What does the singer say he will never do?", index, chunks)
-    print(results)
+    results = embedder.search("What are large language models and how do they work?", index, chunks)
+    response = chat_completion.generate_response("What are large language models and how do they work?", results, [])
+    print(response)
 
 main()
---
+++ backend/tools/chat_completion.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+
+class ChatCompletion:
+    def __init__(self):
+        self.client = OpenAI()
+    
+    def generate_response(self, content: str, chunks: list[str], history: list[str]):
+        response = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", 
+                 "content": """You are a helpful assistant that can answer questions about the content of the video.
+                 You will also be given a list of chunks of the most relecant pars of the transcript.
+                 You will need to use the chunks to answer the question.
+                 You will also be given a list of history of the conversation.
+                 You may need to use the history to answer the question.
+                
+                 
+                 Transcript chunks: {chunks}
+                 History: {history}
+                 """},
+                {"role": "user", "content": content},
+            ]
+        )
+        return response.choices[0].message.content
